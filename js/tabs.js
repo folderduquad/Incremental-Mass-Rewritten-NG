cc3c990598f3f3d9c1482c8cd2e5ae70bc03d193
FD2_4 <plasmolykitten@163.com>
2026-09-13 01:27:57 +0000

    addsth.

--- js/tabs.js
+++ js/tabs.js
@@ -22,7 +22,7 @@ const TABS = {
         { id: "Quantum", icon: "material-symbols:grid-4x4-rounded", color: "lightgreen", unl() { return quUnl() }, style: "qu" },
         { id: "Darkness", icon: "ic:baseline-remove-red-eye", color: "grey", unl() { return player.dark.unl }, style: "dark" },
         { id: "Infinity", icon: "game-icons:infinity", color: "orange", unl() { return tmp.inf_unl }, style: "inf" },
-        { id: "Reincarnation", unl() { return hasInfUpgrade(16) }, icon: "mdi:restart-alt", color: "cyan", style: "reinc" },
+        { id: "Reincarnation", unl() { return hasInfUpgrade(16) }, icon: "mdi:restart-alt", color: "green", style: "reinc" },
         { id: "Options", icon: "mdi:gear" }, 
     ],
     2: {
